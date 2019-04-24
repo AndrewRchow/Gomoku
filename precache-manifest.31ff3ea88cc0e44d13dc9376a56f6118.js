fdf3a81e22a13af4ad95c0a602bdc4afec7e5813
Andrew Chow <chowlick@gmail.com>
2019-04-24 10:02:35 -0700

    Updates

---
+++ precache-manifest.31ff3ea88cc0e44d13dc9376a56f6118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44497014c78d3c7e1d5c",
+    "url": "/gomoku/static/css/main.0f8cc7ae.chunk.css"
+  },
+  {
+    "revision": "44497014c78d3c7e1d5c",
+    "url": "/gomoku/static/js/main.50f296e6.chunk.js"
+  },
+  {
+    "revision": "cae0c02a0b91c122477b",
+    "url": "/gomoku/static/js/runtime~main.526c671c.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/gomoku/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "62925d5bea262ce4754e597707d05908",
+    "url": "/gomoku/index.html"
+  }
+];
